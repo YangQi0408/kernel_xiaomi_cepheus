22dad05549fba08f73f701b4fe3d46614a7c175d
YangQi0408 <a123er3643@163.com>
2025-06-26 22:05:33 +0800

    Update

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 ANYKERNEL3_DIR=$PWD/AnyKernel/
 FINAL_KERNEL_ZIP=InfiniR_cepheus_v1.39_KSUN.zip
 
-TOOLCHAIN_PATH="/home/yangqi/toolchains/clang-r416183b1/bin"
+TOOLCHAIN_PATH="/home/yangqi/toolchains/proton-clang/bin"
 
 export LLVM=1
 export USE_CCACHE=1
@@ -32,11 +32,11 @@ MAKE_ARGS="AR=llvm-ar \
         CROSS_COMPILE_COMPAT=arm-linux-gnueabi- \
         CLANG_TRIPLE=aarch64-linux-gnu-"
 
-make CC="ccache clang" CXX="ccache clang++" $MAKE_ARGS cepheus_defconfig -j12
+make AS=as CC="ccache clang" CXX="ccache clang++" $MAKE_ARGS cepheus_defconfig -j12
 
 START=$(date +"%s")
 
-make CC="ccache clang" CXX="ccache clang++" $MAKE_ARGS -j12
+make AS=as CC="ccache clang" CXX="ccache clang++" $MAKE_ARGS -j12
 
 echo -e "$yellow**** Verify Image.gz-dtb ****$nocol"
 ls $PWD/out/arch/arm64/boot/Image.gz-dtb
@@ -53,7 +53,7 @@ cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
 echo -e "$yellow**** Time to zip up! ****$nocol"
 cd $ANYKERNEL3_DIR/
 zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
-cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP /home/raystef66/kernel/$FINAL_KERNEL_ZIP
+cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP /home/yangqi/kernel/$FINAL_KERNEL_ZIP
 
 echo -e "$yellow**** Done, here is your checksum ****$nocol"
 cd ..
